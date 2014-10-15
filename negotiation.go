397e3afae2dc37d5f9e7a0872509c84afa882df6
Kévin Gomez <geek63@gmail.com>
2014-10-15 07:27:46 +0000

    Add format classes support

--- negotiation.go
+++ negotiation.go
@@ -1,18 +1,22 @@
 package negotiation
 
 import (
-  "errors"
-  "sort"
-  "strconv"
-  "strings"
+	"errors"
+	"sort"
+	"strconv"
+	"strings"
 )
 
-type Alternative struct {
-  Value   string
-  Quality float64
-  Params  map[string]string
+var formats = map[string][]string{}
+var reverseFormats = map[string]string{}
 
-  mainType, subType string
+type Alternative struct {
+	Name    string
+	Value   string
+	Quality float64
+	Params  map[string]string
+
+	mainType, subType string
 }
 
 // just to ease alternatives sorting
@@ -25,177 +29,212 @@ type MediaTokenizer func(string) (string, string, error)
 type QualityEvaluator func(*Alternative)
 
 func LanguageTokenizer(text string) (string, string, error) {
-  typeInfos := strings.Split(text, "-")
+	typeInfos := strings.Split(text, "-")
 
-  if len(typeInfos) == 2 {
-    return typeInfos[0], typeInfos[1], nil
-  }
+	if len(typeInfos) == 2 {
+		return typeInfos[0], typeInfos[1], nil
+	}
 
-  return typeInfos[0], "*", nil
+	return typeInfos[0], "*", nil
 }
 
 func DefaultTokenizer(text string) (string, string, error) {
-  typeInfos := strings.Split(text, "/")
+	typeInfos := strings.Split(text, "/")
 
-  if len(typeInfos) != 2 {
-    return "", "", errors.New("Impossible to parse media-type " + text)
-  }
+	if len(typeInfos) != 2 {
+		return "", "", errors.New("Impossible to parse media-type " + text)
+	}
 
-  return typeInfos[0], typeInfos[1], nil
+	return typeInfos[0], typeInfos[1], nil
 }
 
 func DefaultQualityEvaluator(alternative *Alternative) {
-  if alternative.mainType == "*" {
-    alternative.Quality = 0.01
-  } else if alternative.subType == "*" {
-    alternative.Quality = 0.02
-  }
+	if alternative.mainType == "*" {
+		alternative.Quality = 0.01
+	} else if alternative.subType == "*" {
+		alternative.Quality = 0.02
+	}
 }
 
 func LanguageQualityEvaluator(alternative *Alternative) {
-  if alternative.mainType == "*" {
-    alternative.Quality = 0.01
-  }
+	if alternative.mainType == "*" {
+		alternative.Quality = 0.01
+	}
 }
 
 func (alternatives alternativeList) Len() int {
-  slice := []Alternative(alternatives)
-  return len(slice)
+	slice := []Alternative(alternatives)
+	return len(slice)
 }
 
 func (alternatives alternativeList) Less(i, j int) bool {
-  slice := []Alternative(alternatives)
-  ai, aj := slice[i], slice[j]
+	slice := []Alternative(alternatives)
+	ai, aj := slice[i], slice[j]
 
-  if ai.Quality > aj.Quality {
-    return true
-  }
+	if ai.Quality > aj.Quality {
+		return true
+	}
 
-  return false
+	return false
 }
 
 func (alternatives alternativeList) Swap(i, j int) {
-  slice := []Alternative(alternatives)
-  slice[i], slice[j] = slice[j], slice[i]
+	slice := []Alternative(alternatives)
+	slice[i], slice[j] = slice[j], slice[i]
 }
 
 func parseHeader(header string, mediaTokenizer MediaTokenizer, qualityEvaluator QualityEvaluator) (alternativeList, error) {
-  parts := strings.Split(header, ",")
-  var alternatives alternativeList = make([]Alternative, 0, len(parts))
-  var err error
+	parts := strings.Split(header, ",")
+	var alternatives alternativeList = make([]Alternative, 0, len(parts))
+	var err error
 
-  for _, part := range parts {
-    // create the new alternative
-    a := Alternative{}
-    a.Quality = 1.0
-    a.Params = make(map[string]string)
+	for _, part := range parts {
+		// create the new alternative
+		a := Alternative{}
+		a.Quality = 1.0
+		a.Params = make(map[string]string)
 
-    // find the value/media-range
-    mediaRange := strings.Split(part, ";")
-    a.Value = strings.Trim(mediaRange[0], " ")
+		// find the value/media-range
+		mediaRange := strings.Split(part, ";")
+		a.Value = strings.Trim(mediaRange[0], " ")
 
-    // parse its type and sub-type
-    a.mainType, a.subType, err = mediaTokenizer(a.Value)
-    if err != nil {
-      return alternatives, err
-    }
+		// parse its type and sub-type
+		a.mainType, a.subType, err = mediaTokenizer(a.Value)
+		if err != nil {
+			return alternatives, err
+		}
 
-    // fix default quality for catch-all values
-    qualityEvaluator(&a)
+		// fix default quality for catch-all values
+		qualityEvaluator(&a)
 
-    // parse the parameters
-    for _, param := range mediaRange[1:] {
-      split := strings.SplitN(strings.Trim(param, " "), "=", 2)
+		// parse the parameters
+		for _, param := range mediaRange[1:] {
+			split := strings.SplitN(strings.Trim(param, " "), "=", 2)
 
-      // ignore invalid splits
-      if len(split) != 2 {
-        continue
-      }
+			// ignore invalid splits
+			if len(split) != 2 {
+				continue
+			}
 
-      key := strings.Trim(split[0], " ")
-      value := strings.Trim(split[1], " ")
+			key := strings.Trim(split[0], " ")
+			value := strings.Trim(split[1], " ")
 
-      // the quality is stored in the parameters, so extract if from them
-      if key == "q" {
-        a.Quality, _ = strconv.ParseFloat(value, 64)
-      } else {
-        a.Params[key] = value
-      }
-    }
+			// the quality is stored in the parameters, so extract if from them
+			if key == "q" {
+				a.Quality, _ = strconv.ParseFloat(value, 64)
+			} else {
+				a.Params[key] = value
+			}
+		}
 
-    alternatives = append(alternatives, a)
-  }
+		alternatives = append(alternatives, a)
+	}
 
-  sort.Stable(alternatives)
+	sort.Stable(alternatives)
 
-  return alternatives, nil
+	return alternatives, nil
 }
 
 func (a Alternative) matches(alternative string, mediaTokenizer MediaTokenizer) bool {
-  if a.Value == alternative {
-    return true
-  }
+	if a.Value == alternative {
+		return true
+	}
 
-  mainType, subType, err := mediaTokenizer(alternative)
-  if err != nil {
-    return false
-  }
+	mainType, subType, err := mediaTokenizer(alternative)
+	if err != nil {
+		return false
+	}
 
-  if mainType == a.mainType && subType == "*" {
-    return true
-  }
+	if mainType == a.mainType && subType == "*" {
+		return true
+	}
 
-  if mainType == a.mainType && a.subType == "*" {
-    return true
-  }
+	if mainType == a.mainType && a.subType == "*" {
+		return true
+	}
 
-  if a.mainType == "*" && a.subType == "*" {
-    return true
-  }
+	if a.mainType == "*" && a.subType == "*" {
+		return true
+	}
 
-  return false
+	return false
 }
 
 func findMatch(alternatives []string, headerAlternative Alternative, mediaTokenizer MediaTokenizer) (string, bool) {
-  for _, alternative := range alternatives {
-    if headerAlternative.matches(alternative, mediaTokenizer) {
-      return alternative, true
-    }
-  }
+	for _, alternative := range alternatives {
+		if headerAlternative.matches(alternative, mediaTokenizer) {
+			return alternative, true
+		}
+	}
 
-  return "", false
+	return "", false
+}
+
+func expandFormats(acceptedFormats []string, formatsMap map[string][]string) []string {
+	formats := make([]string, 0, len(acceptedFormats))
+
+	for _, format := range acceptedFormats {
+		mapping, ok := formatsMap[format]
+
+		if ok {
+			formats = append(formats, mapping...)
+		} else {
+			formats = append(formats, format)
+		}
+	}
+
+	return formats
+}
+
+func RegisterFormat(name string, mimeTypes []string) {
+	formats[name] = mimeTypes
+
+	for _, mimeType := range mimeTypes {
+		reverseFormats[mimeType] = name
+	}
 }
 
 func Negotiate(header string, acceptedAlternatives []string, mediaTokenizer MediaTokenizer, qualityEvaluator QualityEvaluator) (*Alternative, error) {
-  alternatives, err := parseHeader(header, mediaTokenizer, qualityEvaluator)
+	alternatives, err := parseHeader(header, mediaTokenizer, qualityEvaluator)
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  if len(alternatives) == 0 {
-    return nil, errors.New("No alternative could be extracted from the given header")
-  }
+	if len(alternatives) == 0 {
+		return nil, errors.New("No alternative could be extracted from the given header")
+	}
 
-  for _, header := range alternatives {
-    alternative, ok := findMatch(acceptedAlternatives, header, mediaTokenizer)
+	for _, header := range alternatives {
+		alternative, ok := findMatch(acceptedAlternatives, header, mediaTokenizer)
 
-    if ok {
-      return &Alternative{
-        Value:   alternative,
-        Quality: header.Quality,
-        Params:  header.Params,
-      }, nil
-    }
-  }
+		if ok {
+			var name string
 
-  return nil, errors.New("No matching alternative found")
+			// try to find if there is a custom format associated to the mime-type
+			reverseFormat, ok := reverseFormats[alternative]
+			if ok {
+				name = reverseFormat
+			}
+
+			return &Alternative{
+				Name:    name,
+				Value:   alternative,
+				Quality: header.Quality,
+				Params:  header.Params,
+			}, nil
+		}
+	}
+
+	return nil, errors.New("No matching alternative found")
 }
 
 func NegotiateLanguage(header string, alternatives []string) (*Alternative, error) {
-  return Negotiate(header, alternatives, LanguageTokenizer, LanguageQualityEvaluator)
+	return Negotiate(header, alternatives, LanguageTokenizer, LanguageQualityEvaluator)
 }
 
 func NegotiateAccept(header string, alternatives []string) (*Alternative, error) {
-  return Negotiate(header, alternatives, DefaultTokenizer, DefaultQualityEvaluator)
+	alternatives = expandFormats(alternatives, formats)
+
+	return Negotiate(header, alternatives, DefaultTokenizer, DefaultQualityEvaluator)
 }
--- negotiation_test.go
+++ negotiation_test.go
@@ -3,92 +3,101 @@ package negotiation
 import "testing"
 
 type alternativeTestData struct {
-  header       string
-  alternatives []string
-  choice       string
+	header       string
+	alternatives []string
+	choiceType   string
+	choiceName   string
 }
 
 var pearAcceptHeader = "text/html,application/xhtml+xml,application/xml;q=0.9,text/*;q=0.7,*/*,image/gif; q=0.8, image/jpeg; q=0.6, image/*"
 
 var negotiateAcceptTestData = []alternativeTestData{
-  // valid cases
-  {pearAcceptHeader, []string{"image/gif", "image/png", "application/xhtml+xml", "application/xml", "text/html", "image/jpeg", "text/plain"}, "text/html"},
-  {pearAcceptHeader, []string{"image/gif", "image/png", "application/xhtml+xml", "application/xml", "image/jpeg", "text/plain"}, "application/xhtml+xml"},
-  {pearAcceptHeader, []string{"image/gif", "image/png", "application/xml", "image/jpeg", "text/plain"}, "application/xml"},
-  {pearAcceptHeader, []string{"image/gif", "image/png", "image/jpeg", "text/plain"}, "image/gif"},
-  {pearAcceptHeader, []string{"image/png", "image/jpeg", "text/plain"}, "text/plain"},
-  {pearAcceptHeader, []string{"image/png"}, "image/png"},
-  {pearAcceptHeader, []string{"audio/midi"}, "audio/midi"},
-  {"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", []string{"application/rss+xml", "*/*"}, "application/rss+xml"},
-  {"text/html", []string{"application/rss"}, ""},
-  {"application/*", []string{"application/rss"}, "application/rss"},
-  {"application/rdf+xml;q=0.5,text/html;q=.3", []string{"application/rdf+xml", "text/html"}, "application/rdf+xml"},
-  {"application/rdf+xml;q=0.5,text/html;q=.3", []string{"application/rdf+xml"}, "application/rdf+xml"},
-  {"application/rdf+xml;q=0.5,text/html;q=.3", []string{"text/html"}, "text/html"},
-  {"application/rdf+xml;q=0.5,text/html;q=.5", []string{"text/html"}, "text/html"},
-  {"application/rdf+xml;q=0.5,text/html;q=.5", []string{"application/rdf+xml"}, "application/rdf+xml"},
-  {"image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, */*", []string{"text/html", "application/xhtml+xml", "*/*"}, "text/html"},
+	// valid cases
+	{pearAcceptHeader, []string{"image/gif", "image/png", "application/xhtml+xml", "application/xml", "text/html", "image/jpeg", "text/plain"}, "text/html", "html"},
+	{pearAcceptHeader, []string{"image/gif", "image/png", "application/xhtml+xml", "application/xml", "image/jpeg", "text/plain"}, "application/xhtml+xml", "html"},
+	{pearAcceptHeader, []string{"image/gif", "image/png", "application/xml", "image/jpeg", "text/plain"}, "application/xml", "xml"},
+	{pearAcceptHeader, []string{"image/gif", "image/png", "image/jpeg", "text/plain"}, "image/gif", ""},
+	{pearAcceptHeader, []string{"image/png", "image/jpeg", "text/plain"}, "text/plain", "txt"},
+	{pearAcceptHeader, []string{"image/png"}, "image/png", ""},
+	{pearAcceptHeader, []string{"audio/midi"}, "audio/midi", ""},
+	{"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", []string{"application/rss+xml", "*/*"}, "application/rss+xml", "rss"},
+	{"text/html", []string{"application/rss"}, "", ""},
+	{"application/*", []string{"application/rss"}, "application/rss", "rss"},
+	{"application/rdf+xml;q=0.5,text/html;q=.3", []string{"application/rdf+xml", "text/html"}, "application/rdf+xml", "rdf"},
+	{"application/rdf+xml;q=0.5,text/html;q=.3", []string{"application/rdf+xml"}, "application/rdf+xml", "rdf"},
+	{"application/rdf+xml;q=0.5,text/html;q=.3", []string{"text/html"}, "text/html", "html"},
+	{"application/rdf+xml;q=0.5,text/html;q=.3", []string{"html"}, "text/html", "html"},
+	{"application/rdf+xml;q=0.5,text/html;q=.5", []string{"text/html"}, "text/html", "html"},
+	{"application/rdf+xml;q=0.5,text/html;q=.5", []string{"application/rdf+xml"}, "application/rdf+xml", "rdf"},
+	{"application/rdf+xml;q=0.5,text/html;q=.5", []string{"rdf"}, "application/rdf+xml", "rdf"},
+	{"image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, */*", []string{"text/html", "application/xhtml+xml", "*/*"}, "text/html", "html"},
 }
 
 func TestNegotiateAccept(t *testing.T) {
-  for _, pair := range negotiateAcceptTestData {
-    var result string
-    alternative, _ := NegotiateAccept(pair.header, pair.alternatives)
+	for _, pair := range negotiateAcceptTestData {
+		var resultType, resultName string
+		alternative, _ := NegotiateAccept(pair.header, pair.alternatives)
 
-    if alternative == nil {
-      result = ""
-    } else {
-      result = alternative.Value
-    }
+		if alternative == nil {
+			resultType = ""
+			resultName = ""
+		} else {
+			resultType = alternative.Value
+			resultName = alternative.Name
+		}
 
-    if result != pair.choice {
-      t.Errorf("For \"%v\" expected \"%v\" but got \"%s\"", pair.header, pair.choice, result)
-    }
-  }
+		if resultType != pair.choiceType {
+			t.Errorf("For \"%v\" expected \"%v\" type but got \"%s\"", pair.header, pair.choiceType, resultType)
+		}
+
+		if resultName != pair.choiceName {
+			t.Errorf("For \"%v\" expected \"%v\" name but got \"%s\"", pair.header, pair.choiceName, resultName)
+		}
+	}
 }
 
 var negotiateInvalidTestData = []alternativeTestData{
-  // invalid data
-  {"text", []string{"text/plain"}, ""},
-  {"text/", []string{"text/plain"}, ""},
+	// invalid data
+	{"text", []string{"text/plain"}, "", ""},
+	{"text/", []string{"text/plain"}, "", ""},
 
-  // no match found
-  {"text/plain", []string{"text/html"}, ""},
-  {"text/*", []string{"image/png"}, ""},
+	// no match found
+	{"text/plain", []string{"text/html"}, "", ""},
+	{"text/*", []string{"image/png"}, "", ""},
 }
 
 func TestNegotiateWithInvalidData(t *testing.T) {
-  for _, pair := range negotiateInvalidTestData {
-    _, err := NegotiateAccept(pair.header, pair.alternatives)
+	for _, pair := range negotiateInvalidTestData {
+		_, err := NegotiateAccept(pair.header, pair.alternatives)
 
-    if err == nil {
-      t.Error("error expected")
-    }
-  }
+		if err == nil {
+			t.Error("error expected")
+		}
+	}
 }
 
 var negotiateLanguageTestData = []alternativeTestData{
-  {"en,fr", []string{"en", "fr"}, "en"},
-  {"en; q=0.1, fr; q=0.4, fu; q=0.9, de; q=0.2", []string{"fu", "fr"}, "fu"},
-  {"da, en-gb;q=0.8, en;q=0.7", []string{"da", "en"}, "da"},
-  {"da, en-gb;q=0.8, en;q=0.7, *", []string{"da", "en"}, "da"},
-  {"da, en-gb;q=0.8, en;q=0.7, *", []string{"en"}, "en"},
-  {"fr-FR,en-US;q=0.6,en;q=0.4", []string{"fr"}, "fr"},
+	{"en,fr", []string{"en", "fr"}, "en", ""},
+	{"en; q=0.1, fr; q=0.4, fu; q=0.9, de; q=0.2", []string{"fu", "fr"}, "fu", ""},
+	{"da, en-gb;q=0.8, en;q=0.7", []string{"da", "en"}, "da", ""},
+	{"da, en-gb;q=0.8, en;q=0.7, *", []string{"da", "en"}, "da", ""},
+	{"da, en-gb;q=0.8, en;q=0.7, *", []string{"en"}, "en", ""},
+	{"fr-FR,en-US;q=0.6,en;q=0.4", []string{"fr"}, "fr", ""},
 }
 
 func TestNegotiateLanguage(t *testing.T) {
-  for _, pair := range negotiateLanguageTestData {
-    var result string
-    alternative, _ := NegotiateLanguage(pair.header, pair.alternatives)
+	for _, pair := range negotiateLanguageTestData {
+		var result string
+		alternative, _ := NegotiateLanguage(pair.header, pair.alternatives)
 
-    if alternative == nil {
-      result = ""
-    } else {
-      result = alternative.Value
-    }
+		if alternative == nil {
+			result = ""
+		} else {
+			result = alternative.Value
+		}
 
-    if result != pair.choice {
-      t.Errorf("For \"%v\" expected \"%v\" but got \"%s\"", pair.header, pair.choice, result)
-    }
-  }
+		if result != pair.choiceType {
+			t.Errorf("For \"%v\" expected \"%v\" but got \"%s\"", pair.header, pair.choiceType, result)
+		}
+	}
 }
